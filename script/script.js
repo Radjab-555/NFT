2494dc0883fde6ac9a5aced7f4c19cfedf9348a8
Radzhab Alibekov <radzabalibecov77@gmail.com>
2022-12-04 10:10:54 +0300

    second commit

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
-var coll = document.getElementsByClassName('.collapsible');
+var colls = document.getElementsByClassName('collapsible');
 var i;
    
-for (i = 0; i < coll.length; i++) {
-coll[i].addEventListener(".click", function() {
-    this.classList.toggle("active");
-    var content = fhis.nextElementSidling;
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-    }else{
-        content.style.maxHeight = content.scrollHeight + "px"; 
-    }
-});
+for(let coll of colls) {
+    coll.onclick = function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        }else{
+            content.style.maxHeight = content.scrollHeight + "px"; 
+        }
+    };
 }
-console.log('.collapsible');
+console.log(colls);
